578e98eeb370c1bf6b796138860fe9a29041a35b
Huon Wilson <dbau.pp+github@gmail.com>
2015-05-01 18:18:33 +1000

    Deprecation.

--- travis-cargo.py
+++ travis-cargo.py
@@ -2,11 +2,12 @@
 from __future__ import print_function
 
 if __name__ == '__main__':
-    print("""project layout was changed. Use one of the following instead:
-- to invoke the script with `travis-cargo`:
-    pip install git+https://github.com/huonw/travis-cargo.git --user $USER && export PATH=$HOME/.local/bin:$PATH
+    print("""project layout was changed. Use the following instead:
 
-- to invoke the script with `./tc`:
-    pip install git+https://github.com/huonw/travis-cargo.git --user $USER && ln -s $HOME/.local/bin/travis-cargo tc
+    pip install 'travis-cargo<0.2' --user && export PATH=$HOME/.local/bin:$PATH
+
+See these links for more info:
+- https://github.com/huonw/travis-cargo
+- http://huonw.github.io/blog/2015/05/travis-on-the-train-part-2/#breaking-change
 """)
     exit(1)

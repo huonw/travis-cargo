f20e9408c1fb35d19fb89d1dea26758a5e94ebb5
Huon Wilson <dbau.pp+github@gmail.com>
2015-05-01 16:47:27 +1000

    Add metadata for PyPI.

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
+CLASSIFIERS = [
+    'License :: OSI Approved :: Apache Software License',
+    'License :: OSI Approved :: MIT License',
+    'Topic :: Software Development :: Testing',
+    'Programming Language :: Other',
+]
 
 setup(
     name = 'travis-cargo',
-    version = '0.0.1',
+    version = '0.1.0',
     description = ('Manages interactions between travis and cargo/rust compilers.'),
+    author = 'Huon Wilson',
+    author_email = 'dbau.pp@gmail.com',
+    url = 'https://github.com/huonw/travis-cargo',
+    download_url = 'https://github.com/huonw/travis-cargo/tarball/0.1.0',
+    keywords = ['testing', 'travis', 'rust', 'cargo'],
+    packages = find_packages(),
     py_modules = ['travis_cargo'],
     entry_points = {
         'console_scripts': ['travis-cargo=travis_cargo:main']
     },
+    classifiers = CLASSIFIERS,
 )

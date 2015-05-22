fa45df5724a39d7caa678864d59646c2052e5942
Huon Wilson <dbau.pp+github@gmail.com>
2015-05-22 22:30:51 +1000

    Release 0.1.2.

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ CLASSIFIERS = [
 
 setup(
     name = 'travis-cargo',
-    version = '0.1.1',
+    version = '0.1.2',
     description = ('Manages interactions between travis and cargo/rust compilers.'),
     author = 'Huon Wilson',
     author_email = 'dbau.pp@gmail.com',
     url = 'https://github.com/huonw/travis-cargo',
-    download_url = 'https://github.com/huonw/travis-cargo/tarball/0.1.1',
+    download_url = 'https://github.com/huonw/travis-cargo/tarball/0.1.2',
     keywords = ['testing', 'travis', 'rust', 'cargo'],
     packages = find_packages(),
     py_modules = ['travis_cargo'],
